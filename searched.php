7e207ed730d26f22c17231796e5425b2b4052aa4
rajatsingh25aug <rajatsingh9bdpsalld@gmail.com>
2016-11-10 03:00:27 +0530

    Add files via upload

---
+++ searched.php
@@ -0,0 +1,13 @@
+<?php
+$un=$_POST["usr"];
+$c=mysql_connect("localhost","root","");
+mysql_select_db("Registration",$c);
+ $res=mysql_query("select * from register where Name='".$un."'");
+ echo "<table border=2 align='center'>";
+ echo "<tr><td><h3>Name</h3></td><td><h3>Course</td></tr>";
+  while( $ar=mysql_fetch_assoc($res))
+  {
+      echo "<tr><td>".$ar["Name"]."</td><td>". $ar["Course"]."</td></tr>";
+  }
+  echo "</table>";
+ ?>
---
+++ show.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="user.php" method="POST">
+<table align="center" border="5">
+<tr><td>USERNAME</td><td><input type="text" name="usr"></td></tr>
+<tr><td colspan="2" ><input type="submit" value="show users" style="width:100%;"></td></tr>
+</table>
+</form>
+</html>
